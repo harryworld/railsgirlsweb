08a865b872c38905330d6172d9c46f155373c3f3
Harry Ng <harryworld@gmail.com>
2014-01-10 23:50:06 +0800

    1. Creating the application
    创建一个应用程序

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirlsweb::Application.config.secret_key_base = '4d8a20ee71bb6cb248cb23a165ddd16fa4286f1a0d52b675e727fa4d6c2c84b2c2b5886844c00f63997a237038544027f2c07a0d24e6f270f6c5a3ff27dcfc20'
